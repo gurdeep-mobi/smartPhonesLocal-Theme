0390637bd9ef76ece298e48a39b17bcce81ecda9
gurdeep-mobi <gurdeep@mobikasa.com>
2024-02-16 13:21:30 +0530

    email changes

--- ekommart-child/woocommerce/emails/customer-processing-order.php
+++ ekommart-child/woocommerce/emails/customer-processing-order.php
@@ -10,43 +10,71 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates\Emails
+ * @see         https://docs.woocommerce.com/document/template-structure/
+ * @author      WooThemes
+ * @package     WooCommerce/Templates/Emails
  * @version 3.7.0
  */
 
+/**
+ * NOTES ABOUT TEMPLATE EDIT FOR KADENCE WOOMAIL DESIGNER, 
+ * 1. add hook 'kadence_woomail_designer_email_details' to pull in main text
+ * 2. Remove static main text area.
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+    exit;
 }
 
-/*
+/**
  * @hooked WC_Emails::email_header() Output the email header
  */
-do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
-
-<?php /* translators: %s: Customer first name */ ?>
-<p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $order->get_billing_first_name() ) ); ?></p>
-<?php /* translators: %s: Order number */ ?>
-<p><?php printf( esc_html__( 'Just to let you know &mdash; we\'ve received your order #%s, and it is now being processed:', 'woocommerce' ), esc_html( $order->get_order_number() ) ); ?></p>
-
-<p>Please print off the prepaid shipping label using the button below or via the attachment to this email to send in your device(s).</p>
-
-<p>Please bring your packaged and ready-to-ship item to your nearest Post office and show the Label Broker Code below to the USPS Retal Associate at the Retail Counter to print the label. </p>
-
-<p style="text-align: center;"><a href="<?php echo home_url(); ?>/wp-content/uploads/shipping_labels/<?php echo $order->get_order_number(); ?>_return_label_raw.pdf" target="_blank" style="background-color:#e06901!important; border-color:#e06901!important; color:#fff; text-decoration: none; border: 2px solid;border-radius: 3px; background: 0 0; cursor: pointer; padding: 0.6180469716em 1.41575em;font-weight: 700;text-shadow: none;display: inline-block;-webkit-appearance: none;">Print Shipping Label</a></p>
-
-<p style="text-align:center;"><img src="<?php echo home_url(); ?>/wp-content/uploads/shipping_labels/<?php echo $order->get_order_number(); ?>_qrcode.png" /></p>
-
-<?php
-$label_id = get_post_meta( $order->get_id(), 'usps_qr_label_id', true );
-$billing_zipcode = $order->get_billing_postcode();
+do_action( 'woocommerce_email_header', $email_heading, $email ); 
 ?>
-<p>You can find your Post Office location <a href="https://tools.usps.com/find-location.htm?locationType=po&serviceType=lbroretail&address=<?php echo $billing_zipcode; ?>">here</a></p>
-<p>You can also access your label <a href="https://tools.usps.com/label-broker.htm">online.</a></p>
+<?php
+$order_id = $order->get_id();
+$label_id = get_post_meta( $order_id, 'usps_qr_label_id', true );
+$billing_zipcode = $order->get_billing_postcode();
 
+$shipping_track_number = '';
+$tracking_items = get_post_meta( $order_id, '_wc_shipment_tracking_items', true );
+if(isset($tracking_items[0]['tracking_number'])){
+        $shipping_track_number = $tracking_items[0]['tracking_number'];
+}
+?>
+<p>
+    <span>No printer for the shipping label? No problem!</span>
+    <ul>
+        <li>Bring your packaged device(s) to the USPS location. To find your most convienient location use the <a href="https://tools.usps.com/find-location.htm?locationType=po&serviceType=lbroretail&address=<?php echo $billing_zipcode; ?>">USPS store locator.</a></li>
+        <li>Show the QR code below to the USPS associate or use the self service kiosk. They will print your shipping label and ship your device(s) for free.</li>
+        <li>We'll update you when your package has been received and inspected.</li>
+    </ul>
+</p>
 
-<p>If you requested packaging material expect to receive them within 3-6 business days. The same shipping label attached will be included in the package for you to use.</p>
+<p style="text-align: center;"></p>
 
+<p style="text-align:center;">
+    <img src="<?php echo home_url(); ?>/wp-content/uploads/shipping_labels/<?php echo $order->get_order_number(); ?>_qrcode.png" />
+</p>
+<p style="text-align:center;">
+    <span style="padding: 0.6180469716em 1.41575em;">
+        <a href="https://tools.usps.com/find-location.htm?locationType=po&serviceType=lbroretail&address=<?php echo $billing_zipcode; ?>" target="_blank">
+          <img src="https://smartphonesstg.wpengine.com/wp-content/uploads/2020/02/Find-post-office.png" />
+        </a>
+    </span>
+    <span style="padding: 0.6180469716em 1.41575em;">
+        <a href="<?php echo home_url(); ?>/wp-content/uploads/shipping_labels/<?php echo $order->get_order_number(); ?>_return_label_raw.pdf" target="_blank" >
+            <img src="https://smartphonesstg.wpengine.com/wp-content/uploads/2020/02/Return-Label.png" />
+        </a>
+    </span>
+</p>
+<?php if(!empty($shipping_track_number)){ ?>
+<p style="text-align:center;">
+    <b>USPS Tracking number <u style="color:darkblue;"><?php echo $shipping_track_number; ?></u> 
+    </b>
+</p>
+<?php } ?>
+<p>If packaging material was requested, expect delivery within 3-6 business days. The shipping label will be included.</p>
 <p>Payment is sent once the shipment is received in our facility and inspected.</p>
 
 <p>
@@ -64,8 +92,12 @@ $billing_zipcode = $order->get_billing_postcode();
     To add insurance the USPS will need to create a new label for you. We will include a $5.00 shipping credit to your order if the label we provided is not used.<br />
 </p>
 <?php
+/**
+ * @hooked Kadence_Woomail_Designer::email_main_text_area
+ */
+do_action( 'kadence_woomail_designer_email_details', $order, $sent_to_admin, $plain_text, $email );
 
-/*
+/**
  * @hooked WC_Emails::order_details() Shows the order details table.
  * @hooked WC_Structured_Data::generate_order_data() Generates structured data.
  * @hooked WC_Structured_Data::output_structured_data() Outputs structured data.
@@ -73,25 +105,26 @@ $billing_zipcode = $order->get_billing_postcode();
  */
 do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
 
-/*
+/**
  * @hooked WC_Emails::order_meta() Shows order meta data.
  */
 do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
 
-/*
+/**
  * @hooked WC_Emails::customer_details() Shows customer details
  * @hooked WC_Emails::email_address() Shows email address
  */
 do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
 
 /**
- * Show user-defined additional content - this is set in each email's settings.
+ * Show user-defined additonal content - this is set in each email's settings.
  */
-if ( $additional_content ) {
-	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+$additional_enable = Kadence_Woomail_Customizer::opt( 'additional_content_enable' );
+if ( isset( $additional_content ) && ! empty( $additional_content ) && apply_filters( 'kadence_email_customizer_additional_enable', $additional_enable, $email ) ) {
+    echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
 }
 
-/*
+/**
  * @hooked WC_Emails::email_footer() Output the email footer
  */
 do_action( 'woocommerce_email_footer', $email );
